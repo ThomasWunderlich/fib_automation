e09428b7dc07d78c14aa820f231e909e8a089ef7
Thomas Wunderlich <twunde@gmail.com>
2024-04-22 10:18:54 -0400

    Initial networking setup

---
+++ tf/networking/main.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Service = "Fibonacci"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "fibbonaci"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+
+  # turn on only 1 nat gateway, since this is a POC
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  enable_vpn_gateway = false
+
+  tags = local.tags
+}
